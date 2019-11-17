f48ee6a654dc44ae2dbd6e47dec770ef37fb8f41
guilhem <guilhem.marchand@gmail.com>
2019-11-17 21:28:05 +0000

    Version 1.0.22
    - feature: Whitelisting major improvement with UI supported and driven whitelisting of indexes at data discovery and search time (Issue #27)
    - feature: Improve builtin choices for time input link selection within modal windows
    - feature: Abstract tracker saved searches to remove useless code redundancy
    - fix: Remove auto-refresh search link for searches which shouldn't be refreshed automatically (audit changes & flip, various collection management)
    - fix: Drilldown on any priority entities in alert should define monitored_state to enabled
    - fix: Monitor split share percentage error (Single forms shall share 25% each)
    - fix: Lagging class auditing can register an incorrect type of operation
    - fix: All time time range picker will not work for audit changes & status flipping
    - fix: Auto refresh set to none has random side effects on embedded chart loading, fixed by none set to long period
    - fix: Switched from default last 7 days to last 24 hours in audit and status flipping UIs
    - fix: TrackMe Mobile view does not honour blacklists

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ app="trackme"
 version=`grep 'version =' trackme/default/app.conf | awk '{print $3}' | sed 's/\.//g'`
 
 rm -f *.tgz
+find . -name "*.pyc" -type f | xargs rm -f
+find . -name "*.py" -type f | xargs chmod go-x
+find trackme/lib -name "*.py" -type f | xargs chmod a-x
 tar -czf ${app}_${version}.tgz --exclude=trackme/local --exclude=trackme/metadata/local.meta --exclude=trackme/lookups/lookup_file_backups trackme
 echo "Wrote: ${app}_${version}.tgz"
 
