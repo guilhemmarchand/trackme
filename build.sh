3d64201d7085a973535915234c09f5a4d7459c2c
Guilhem Marchand <gmarchand@splunk.com>
2020-03-06 21:48:16 +0000

    Version 1.1.13
    - feature: Introducing inter-updates automatic refresh, operations that involve updates (modification of the max lag, etc) now dynamically refresh the entity drilldown view and related tokens, which prevents and automatically fixes conflicts during entity updates within the UI.
    - feature: Introducing on demand auto determination of entity max lagging for data sources and hosts, based on either the percentile 95 or average lagging observed for that entity
    - fix: minor fixes and code improvements

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ rm -f *.tgz
 find . -name "*.pyc" -type f | xargs rm -f
 find . -name "*.py" -type f | xargs chmod go-x
 find trackme/lib -name "*.py" -type f | xargs chmod a-x
-tar -cvzf ${app}_${version}.tgz --exclude=trackme/local --exclude=trackme/metadata/local.meta --exclude=trackme/lookups/lookup_file_backups --exclude='./.*'  --exclude='.[^/]*' --exclude="._*" trackme
+tar -czf ${app}_${version}.tgz --exclude=trackme/local --exclude=trackme/metadata/local.meta --exclude=trackme/lookups/lookup_file_backups --exclude='./.*'  --exclude='.[^/]*' --exclude="._*" trackme
 echo "Wrote: ${app}_${version}.tgz"
 
 exit 0
