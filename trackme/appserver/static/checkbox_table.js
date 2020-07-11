3c6eeeaf7b5b5df3ec5d156640deeae16b5a134b
Guilhem Marchand <guilhem.marchand@gmail.com>
2020-07-11 16:41:58 +0100

    Work in progress

--- trackme/appserver/static/checkbox_table.js
+++ trackme/appserver/static/checkbox_table.js
@@ -21,6 +21,7 @@ require([
      var selected_values_array12 = [];
      var selected_values_array13 = [];
      var selected_values_array14 = [];
+     var selected_values_array15 = [];
      var submittedTokens = mvc.Components.get('submitted');
 
      // Table1
@@ -635,6 +636,50 @@ require([
              sh.getVisualization(function(tableView) {
                  // Add custom cell renderer and force re-render
                  tableView.table.addCellRenderer(new CustomRangeRenderer14());
+                 tableView.table.render();
+             });
+         }
+     }
+
+     // Table15
+
+     var CustomRangeRenderer15 = TableView.BaseCellRenderer.extend({
+         canRender: function(cell) {
+             return _(['select']).contains(cell.field);
+         },
+         render: function($td, cell) {
+             var a = $('<div>').attr({"id":"chk-elastic-source-shared-policy"+cell.value,"value":cell.value}).addClass('checkbox').click(function() {
+                 // console.log("checked",$(this).attr('class'));
+                 // console.log("checked",$(this).attr('value'));
+                 if($(this).attr('class')==="checkbox")
+                 {
+                     selected_values_array15.push($(this).attr('value'));
+                     $(this).removeClass();
+                     $(this).addClass("checkbox checked");
+                 }
+                 else {
+                     $(this).removeClass();
+                     $(this).addClass("checkbox");
+                     var i = selected_values_array15.indexOf($(this).attr('value'));
+                     if(i != -1) {
+                         selected_values_array15.splice(i, 1);
+                     }
+                 }
+                 console.log(selected_values_array15);
+                 tokens.set("removeElasticSourceShared", selected_values_array15.join());
+                 submittedTokens.set(tokens.toJSON());
+             }).appendTo($td);
+         }
+     });
+
+     //List of table IDs
+     var tableIDs = ["tableElasticSources"];
+     for (i=0;i<tableIDs.length;i++) {
+         var sh = mvc.Components.get(tableIDs[i]);
+         if(typeof(sh)!="undefined") {
+             sh.getVisualization(function(tableView) {
+                 // Add custom cell renderer and force re-render
+                 tableView.table.addCellRenderer(new CustomRangeRenderer15());
                  tableView.table.render();
              });
          }
