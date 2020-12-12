4c6aac32e0d9422e42fa14e85d19de78f608e697
Guilhem Marchand <guilhem.marchand@gmail.com>
2020-12-12 09:50:22 +0000

    Work in progress

--- trackme/bin/trackme_rest_handler_data_sources.py
+++ trackme/bin/trackme_rest_handler_data_sources.py
@@ -1136,3 +1136,153 @@ class TrackMeHandlerDataSources_v1(rest_handler.RESTHandler):
                 'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
             }
 
+    # Update monitoring level by object name
+    def post_ds_update_outliers_by_name(self, request_info, **kwargs):
+
+        # By data_name
+        data_name = None
+        query_string = None
+
+        # Retrieve from data
+        resp_dict = json.loads(str(request_info.raw_args['payload']))
+        data_name = resp_dict['data_name']
+        OutlierMinEventCount = resp_dict['OutlierMinEventCount'] # integer, default to 0 (disabled)
+        OutlierLowerThresholdMultiplier = resp_dict['OutlierLowerThresholdMultiplier'] # integer, defaults to 4
+        OutlierUpperThresholdMultiplier = resp_dict['OutlierUpperThresholdMultiplier'] # integer, defaults to 4
+        OutlierAlertOnUpper = resp_dict['OutlierAlertOnUpper'] # true / false
+        OutlierTimePeriod = resp_dict['OutlierTimePeriod'] # relative time period, default to -7d
+        OutlierSpan = resp_dict['OutlierSpan'] # span period Splunk notation, defaults to 5m
+        enable_behaviour_analytic = resp_dict['enable_behaviour_analytic'] # true / false
+
+        # Update comment is optional and used for audit changes
+        try:
+            update_comment = resp_dict['update_comment']
+        except Exception as e:
+            update_comment = "API update"
+
+        # Define the KV query
+        query_string = '{ "data_name": "' + data_name + '" }'
+        
+        # Get splunkd port
+        entity = splunk.entity.getEntity('/server', 'settings',
+                                            namespace='trackme', sessionKey=request_info.session_key, owner='-')
+        splunkd_port = entity['mgmtHostPort']
+
+        try:
+
+            # Data collection
+            collection_name = "kv_trackme_data_source_monitoring"            
+            service = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection = service.kvstore[collection_name]
+
+            # Audit collection
+            collection_name_audit = "kv_trackme_audit_changes"            
+            service_audit = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection_audit = service_audit.kvstore[collection_name_audit]
+
+            # Get the current record
+            # Notes: the record is returned as an array, as we search for a specific record, we expect one record only
+            
+            try:
+                record = collection.data.query(query=str(query_string))
+                key = record[0].get('_key')
+
+            except Exception as e:
+                key = None
+                
+            # Render result
+            if key is not None and len(key)>2:
+
+                # Update the record
+                collection.data.update(str(key), json.dumps({                    
+                    "object_category": record[0].get('object_category'), 
+                    "data_index": record[0].get('data_index'),
+                    "data_last_lag_seen": record[0].get('data_last_lag_seen'),
+                    "data_last_ingestion_lag_seen": record[0].get('data_last_ingestion_lag_seen'),
+                    "data_eventcount": record[0].get('data_eventcount'),
+                    "data_last_lag_seen_idx": record[0].get('data_last_lag_seen_idx'),
+                    "data_first_time_seen": record[0].get('data_first_time_seen'),
+                    "data_last_time_seen": record[0].get('data_last_time_seen'),
+                    "data_last_ingest": record[0].get('data_last_ingest'),
+                    "data_last_time_seen_idx": record[0].get('data_last_time_seen_idx'),
+                    "data_max_lag_allowed": record[0].get('data_max_lag_allowed'),
+                    "data_lag_alert_kpis": record[0].get('data_lag_alert_kpis'),
+                    "data_monitored_state": record[0].get('data_monitored_state'),
+                    "data_name": record[0].get('data_name'),
+                    "data_sourcetype": record[0].get('data_sourcetype'),
+                    "data_monitoring_level": record[0].get('data_monitoring_level'),
+                    "data_monitoring_wdays": record[0].get('data_monitoring_wdays'),
+                    "data_override_lagging_class": record[0].get('data_override_lagging_class'),
+                    "data_source_state": record[0].get('data_source_state'),
+                    "data_tracker_runtime": record[0].get('data_tracker_runtime'),
+                    "data_previous_source_state": record[0].get('data_previous_source_state'),
+                    "data_previous_tracker_runtime": record[0].get('data_previous_tracker_runtime'),
+                    "dcount_host": record[0].get('dcount_host'),
+                    "min_dcount_host": record[0].get('min_dcount_host'),
+                    "OutlierMinEventCount": str(OutlierMinEventCount),
+                    "OutlierLowerThresholdMultiplier": str(OutlierLowerThresholdMultiplier),
+                    "OutlierUpperThresholdMultiplier": str(OutlierUpperThresholdMultiplier),
+                    "OutlierAlertOnUpper": str(OutlierAlertOnUpper),
+                    "OutlierTimePeriod": str(OutlierTimePeriod),
+                    "OutlierSpan": str(OutlierSpan),
+                    "isOutlier": record[0].get('isOutlier'),
+                    "enable_behaviour_analytic": str(enable_behaviour_analytic),
+                    "isAnomaly": record[0].get('isAnomaly'),
+                    "data_sample_lastrun": record[0].get('data_sample_lastrun'),
+                    "tags": record[0].get('tags'),
+                    "latest_flip_state": record[0].get('latest_flip_state'),
+                    "latest_flip_time": record[0].get('latest_flip_time'),
+                    "priority": record[0].get('priority')
+                    }))
+
+                # Record an audit change
+                import time
+                current_time = int(round(time.time() * 1000))
+                user = "nobody"
+
+                try:
+
+                    # Insert the record
+                    collection_audit.data.insert(json.dumps({                        
+                        "time": str(current_time),
+                        "user": str(user),
+                        "action": "success",
+                        "change_type": "modify outliers",
+                        "object": str(data_name),
+                        "object_category": "data_source",
+                        "object_attrs": str(json.dumps(collection.data.query_by_id(key), indent=1)),
+                        "result": "N/A",
+                        "comment": str(update_comment)
+                        }))
+
+                except Exception as e:
+                    return {
+                        'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+                    }
+
+                return {
+                    "payload": json.dumps(collection.data.query_by_id(key), indent=1),
+                    'status': 200 # HTTP status code
+                }
+
+            else:
+                return {
+                    "payload": 'Warn: resource not found or request is incorrect ' + str(query_string),
+                    'status': 404 # HTTP status code
+                }
+
+        except Exception as e:
+            return {
+                'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+            }
+
