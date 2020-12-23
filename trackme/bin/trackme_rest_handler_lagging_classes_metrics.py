7c7ae5a91be16e754f142e83bd0e8bfc9c430952
Guilhem Marchand <guilhem.marchand@gmail.com>
2020-12-23 00:49:02 +0000

    Version 1.2.30

---
+++ trackme/bin/trackme_rest_handler_lagging_classes_metrics.py
@@ -0,0 +1,371 @@
+import logging
+import os, sys
+import splunk
+import splunk.entity
+import splunk.Intersplunk
+import json
+
+logger = logging.getLogger(__name__)
+
+splunkhome = os.environ['SPLUNK_HOME']
+sys.path.append(os.path.join(splunkhome, 'etc', 'apps', 'trackme', 'lib'))
+
+import rest_handler
+import splunklib.client as client
+
+class TrackMeHandlerLaggingClassesMetrics_v1(rest_handler.RESTHandler):
+    def __init__(self, command_line, command_arg):
+        super(TrackMeHandlerLaggingClassesMetrics_v1, self).__init__(command_line, command_arg, logger)
+
+    # Get the entire collection as a Python array
+    def get_lagging_classes_metrics(self, request_info, **kwargs):
+
+        # Get splunkd port
+        entity = splunk.entity.getEntity('/server', 'settings',
+                                            namespace='trackme', sessionKey=request_info.session_key, owner='-')
+        splunkd_port = entity['mgmtHostPort']
+
+        try:
+
+            collection_name = "kv_trackme_metric_lagging_definition"            
+            service = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection = service.kvstore[collection_name]
+
+            # Render
+            return {
+                "payload": json.dumps(collection.data.query(), indent=1),
+                'status': 200 # HTTP status code
+            }
+
+        except Exception as e:
+            return {
+                'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+            }
+
+
+    # Get model
+    def get_lagging_classes_metrics_by_name(self, request_info, **kwargs):
+
+        # By name
+        metric_category = None
+
+        # query_string to find records
+        query_string = None
+
+        # Retrieve from data
+        resp_dict = json.loads(str(request_info.raw_args['payload']))
+        metric_category = resp_dict['metric_category']
+
+        # Define the KV query
+        query_string = '{ "metric_category": "' + metric_category + '" }'
+        
+        # Get splunkd port
+        entity = splunk.entity.getEntity('/server', 'settings',
+                                            namespace='trackme', sessionKey=request_info.session_key, owner='-')
+        splunkd_port = entity['mgmtHostPort']
+
+        try:
+
+            collection_name = "kv_trackme_metric_lagging_definition"            
+            service = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection = service.kvstore[collection_name]
+
+            # Get the current record
+            # Notes: the record is returned as an array, as we search for a specific record, we expect one record only
+            
+            try:
+                record = collection.data.query(query=str(query_string))
+                key = record[0].get('_key')
+
+            except Exception as e:
+                key = None
+
+            # Render result
+            if key is not None and len(key)>2:
+
+                return {
+                    "payload": json.dumps(collection.data.query_by_id(key), indent=1),
+                    'status': 200 # HTTP status code
+                }
+
+            else:
+
+                return {
+                    "payload": 'Warn: resource not found ' + str(key),
+                    'status': 404 # HTTP status code
+                }
+
+        except Exception as e:
+            return {
+                'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+            }
+
+
+    # Add new policy
+    def post_lagging_classes_metrics_add(self, request_info, **kwargs):
+
+        # Declare
+        metric_category = None
+        metric_max_lag_allowed = None
+
+        query_string = None
+
+        # Retrieve from data
+        resp_dict = json.loads(str(request_info.raw_args['payload']))
+        metric_category = resp_dict['metric_category']
+        metric_max_lag_allowed = resp_dict['metric_max_lag_allowed']
+
+        # Update comment is optional and used for audit changes
+        try:
+            update_comment = resp_dict['update_comment']
+        except Exception as e:
+            update_comment = "API update"
+
+        # Define the KV query
+        query_string = '{ "metric_category": "' + metric_category + '" }'
+
+        # Get splunkd port
+        entity = splunk.entity.getEntity('/server', 'settings',
+                                            namespace='trackme', sessionKey=request_info.session_key, owner='-')
+        splunkd_port = entity['mgmtHostPort']
+
+        try:
+
+            # Data collection
+            collection_name = "kv_trackme_metric_lagging_definition"            
+            service = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection = service.kvstore[collection_name]
+
+            # Audit collection
+            collection_name_audit = "kv_trackme_audit_changes"            
+            service_audit = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection_audit = service_audit.kvstore[collection_name_audit]
+
+            # Get the current record
+            # Notes: the record is returned as an array, as we search for a specific record, we expect one record only
+            
+            try:
+                record = collection.data.query(query=str(query_string))
+                key = record[0].get('_key')
+
+            except Exception as e:
+                key = None
+                
+            # Render result
+            if key is not None and len(key)>2:
+
+                # This record exists already
+                metric_category = record[0].get('metric_category')
+
+                # Record an audit change
+                import time
+                current_time = int(round(time.time() * 1000))
+                user = "nobody"
+
+                # Update the record
+                collection.data.update(str(key), json.dumps({"metric_category": metric_category, "metric_max_lag_allowed": metric_max_lag_allowed}))
+
+                # Store the record for audit purposes
+                record = str(json.dumps(collection.data.query_by_id(key), indent=1))
+
+                try:
+
+                    # Insert the record
+                    collection_audit.data.insert(json.dumps({    
+                        "time": str(current_time),
+                        "user": str(user),
+                        "action": "success",
+                        "change_type": "create metric sla policy",
+                        "object": str(name),
+                        "object_category": "metric_sla_policy",
+                        "object_attrs": str(record),
+                        "result": "N/A",
+                        "comment": str(update_comment)
+                        }))
+
+                except Exception as e:
+                    return {
+                        'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+                    }
+
+                return {
+                    "payload": str(record),
+                    'status': 200 # HTTP status code
+                }
+
+            else:
+
+                # This record does not exist yet
+
+                # Record an audit change
+                import time
+                current_time = int(round(time.time() * 1000))
+                user = "nobody"
+
+                # Insert the record
+                collection.data.insert(json.dumps({"metric_category": metric_category, "metric_max_lag_allowed": metric_max_lag_allowed}))
+
+                # Get record
+                record = json.dumps(collection.data.query(query=str(query_string)), indent=1)
+
+                try:
+
+                    # Insert the record
+                    collection_audit.data.insert(json.dumps({    
+                        "time": str(current_time),
+                        "user": str(user),
+                        "action": "success",
+                        "change_type": "create metric sla policy",
+                        "object": str(metric_category),
+                        "object_category": "metric_sla_policy",
+                        "object_attrs": str(record),
+                        "result": "N/A",
+                        "comment": str(update_comment)
+                        }))
+
+                except Exception as e:
+                    return {
+                        'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+                    }
+
+                return {
+                    "payload": str(record),
+                    'status': 200 # HTTP status code
+                }
+
+        except Exception as e:
+            return {
+                'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+            }
+
+
+    # Delete a custom model
+    def delete_lagging_classes_metrics_del(self, request_info, **kwargs):
+
+        # Declare
+        name = None
+        query_string = None
+
+        # Retrieve from data
+        resp_dict = json.loads(str(request_info.raw_args['payload']))
+        metric_category = resp_dict['metric_category']
+
+        # Update comment is optional and used for audit changes
+        try:
+            update_comment = resp_dict['update_comment']
+        except Exception as e:
+            update_comment = "API update"
+
+        # Define the KV query
+        query_string = '{ "metric_category": "' + metric_category + '" }'
+
+        # Get splunkd port
+        entity = splunk.entity.getEntity('/server', 'settings',
+                                            namespace='trackme', sessionKey=request_info.session_key, owner='-')
+        splunkd_port = entity['mgmtHostPort']
+
+        try:
+
+            # Data collection
+            collection_name = "kv_trackme_metric_lagging_definition"            
+            service = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection = service.kvstore[collection_name]
+
+            # Audit collection
+            collection_name_audit = "kv_trackme_audit_changes"            
+            service_audit = client.connect(
+                owner="nobody",
+                app="trackme",
+                port=splunkd_port,
+                token=request_info.session_key
+            )
+            collection_audit = service_audit.kvstore[collection_name_audit]
+
+            # Get the current record
+            # Notes: the record is returned as an array, as we search for a specific record, we expect one record only
+            
+            try:
+                record = collection.data.query(query=str(query_string))
+                key = record[0].get('_key')
+
+            except Exception as e:
+                key = None
+                
+            # Render result
+            if key is not None and len(key)>2:
+
+                # This record exists already
+
+                # Store the record for audit purposes
+                record = str(json.dumps(collection.data.query_by_id(key), indent=1))
+
+                # Record an audit change
+                import time
+                current_time = int(round(time.time() * 1000))
+                user = "nobody"
+
+                try:
+
+                    # Remove the record
+                    collection.data.delete(json.dumps({"_key":key}))
+
+                    # Insert the record
+                    collection_audit.data.insert(json.dumps({    
+                        "time": str(current_time),
+                        "user": str(user),
+                        "action": "success",
+                        "change_type": "delete metric sla policy",
+                        "object": str(metric_category),
+                        "object_category": "metric_sla_policy",
+                        "object_attrs": str(record),
+                        "result": "N/A",
+                        "comment": str(update_comment)
+                        }))
+
+                except Exception as e:
+                    return {
+                        'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+                    }
+
+                return {
+                    "payload": "Record with _key " + str(key) + " was deleted from the collection.",
+                    'status': 200 # HTTP status code
+                }
+
+            else:
+
+                return {
+                    "payload": 'Warn: resource not found ' + str(key),
+                    'status': 404 # HTTP status code
+                }
+
+        except Exception as e:
+            return {
+                'payload': 'Warn: exception encountered: ' + str(e) # Payload of the request.
+            }
+
