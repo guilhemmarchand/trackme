f1d4547a09a41127474285eb77bd23f71c3d861c
Guilhem Marchand <guilhem.marchand@gmail.com>
2021-12-26 10:30:51 +0000

    Verson 1.3.0: candidate 001

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ author = u'Guilhem Marchand'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.2'
+version = u'1.3'
 # The full version, including alpha/beta/rc tags.
 release = u'1'
 
